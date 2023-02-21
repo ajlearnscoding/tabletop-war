77f6636a144996b8120c5ab4f6306ce67dfece78
ajlearnscoding <andyjonesmusic@gmail.com>
2023-02-21 20:23:27 +0000

    updated title for MapUI and added MapEditor

---
+++ src/MapEditor.java
@@ -0,0 +1,10 @@
+public class MapEditor {
+    private int[][] map;
+
+    public MapEditor(int rows, int columns) {
+        map = new int[rows][columns];
+    }
+
+    // Define the methods to interact with the map here
+    
+}
